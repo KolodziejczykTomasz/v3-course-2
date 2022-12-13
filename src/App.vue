e55f2f3fa735411f053ec9bafc77b204882f2452
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2022-12-13 20:10:08 +0100

    Add new actions.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,125 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+ <div>
 
+      <ScoreBoard :win-count="this.winCount" :lose-count="this.loseCount"/>
+
+
+   <template v-if="this.question">
+     <h1 v-html="this.question"></h1>
+     <template v-for="(answer, index) in this.answers" v-bind:key="index">
+       <input
+           :disabled="this.answerSubmitted"
+           type="radio"
+           name="options"
+           :value="answer"
+           v-model="this.chosenAnswer"
+       />
+       <label v-html="answer"></label>
+       <br>
+     </template>
+     <button  v-if="!this.answerSubmitted" @click="this.submitAnswer()"
+              class="send" type="button" >Send
+     </button>
+     <section class="result" v-if="this.answerSubmitted">
+       <h4
+           v-if="this.chosenAnswer === this.correctAnswers"
+           v-html="'&#9989;Congratulations, the answer ' + this.correctAnswers
+     + ' is correct.'"
+       ></h4>
+       <h4 v-else 
+           v-html="'&#10060; I`m  sorry, you picked the wrong answer. The correct is ' + this.correctAnswers + '. '"></h4>
+
+       <button @click="this.getNewQuestion()" class="send" type="button" >Next
+         question
+       </button>
+     </section>
+   </template>
+ </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import ScoreBoard from "@/components/ScoreBoard.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {ScoreBoard},
+  data(){
+    return{
+      question: undefined,
+      incorrectAnswers:undefined,
+      correctAnswers:undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      winCount: 0,
+      loseCount: 0
+    }
+  },
+  computed: {
+     answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+       answers.splice(Math.round(Math.random() * answers.length), 0,
+           this.correctAnswers);
+       return answers;
+     }
+  },
+  methods: {
+    submitAnswer() {
+      if(!this.chosenAnswer) {
+         alert('Pick one of the options');
+      } else {
+        this.answerSubmitted = true;
+        if(this.chosenAnswer === this.correctAnswers){
+            this.winCount++;
+        }else {
+            this.loseCount++;
+        }
+      }
+    },
+    getNewQuestion() {
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+
+      this.axios
+          .get('https://opentdb.com/api.php?amount=1&category=18')
+          .then((response) => {
+            this.question = response.data.results[0].question;
+            this.incorrectAnswers = response.data.results[0].incorrect_answers;
+            this.correctAnswers = response.data.results[0].correct_answer;
+          })
+    }
+  },
+
+  created(){
+   this.getNewQuestion();
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+  
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
